f0e05c4b3a88219ea8ca4cf9573937acece9f119
justalearner93 <58352212+justalearner93@users.noreply.github.com>
2021-12-27 05:03:15 +0200

    Update WorkShiftManagementApplication.java

--- src/main/java/com/example/Work/shift/management/WorkShiftManagementApplication.java
+++ src/main/java/com/example/Work/shift/management/WorkShiftManagementApplication.java
@@ -9,12 +9,6 @@ import com.example.Work.shift.management.entity.Employee;
 import com.example.Work.shift.management.repository.EmployeeRepository;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
- 	@Override
- 	protected SpringApplicationBuilder configure(SpringApplicationBuilder
-	application) {
-		 return application.sources(Application.class);
- 	}
 public class WorkShiftManagementApplication implements CommandLineRunner {
 	
 	public static void main(String[] args) {
